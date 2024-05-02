92b0e6f41d7a29290683183ca01897c56f041caa
VladimirGoncharuk <116846695+VladimirGoncharuk@users.noreply.github.com>
2024-05-02 15:07:54 +0300

    Update CardInfo.jsx

--- src/screens/ResultSearch/components/CardInfo.jsx
+++ src/screens/ResultSearch/components/CardInfo.jsx
@@ -21,7 +21,7 @@ function CardInfo(item) {
     useEffect(() => {
         const html = jObj.scandoc['#text']
         html && setHtml(html)
-    }, [html])
+    }, [jObj.scandoc['#text']])
     return (
         <div className="result__onelist">
             <div className="cardHeader">
